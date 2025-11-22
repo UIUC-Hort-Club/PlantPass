b588eddd7bde2fbf5a2d150aad9b7eb821360c78
Joseph Ku <josephku825@gmail.com>
2025-11-21 19:19:09 -0600

    remove acl

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,6 @@
 # S3 Bucket
 resource "aws_s3_bucket" "frontend" {
   bucket = "plantpass-frontend"
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
-
-  versioning {
-    enabled = true
-  }
 }
 
 # CloudFront Distribution
