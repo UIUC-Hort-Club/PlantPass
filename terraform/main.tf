f8e44998e93c571c46ed7202427e403d1cb6bddb
Joseph Ku <josephku825@gmail.com>
2025-11-21 19:01:09 -0600

    deploy frontend with tf

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+# S3 Bucket
+resource "aws_s3_bucket" "frontend" {
+  bucket = "plantpass-frontend"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "frontend" {
+  enabled             = true
+  default_root_object = "index.html"
+  comment             = "PlantPass React frontend"
+
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "S3-PlantPass"
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-PlantPass"
+    viewer_protocol_policy = "redirect-to-https"
+
+    # required
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
