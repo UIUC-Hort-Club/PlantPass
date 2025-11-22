a29d40e56ca7f97628ac5c2b0d2e825cb31d497c
Joseph Ku <josephku825@gmail.com>
2025-11-21 19:40:39 -0600

    try another name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # S3 Bucket
 resource "aws_s3_bucket" "frontend" {
-  bucket = "plantpass-frontend-gaillardia-pulchella"
+  bucket = "plantpass-hortclubuiuc-frontend-indian-blanket-gaillardia-pulchella"
 }
 
 # CloudFront Distribution
