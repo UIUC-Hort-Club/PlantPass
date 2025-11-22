6b107eda2dbdd53ef49871fa845d1cabc172741e
Joseph Ku <99053428+joku8@users.noreply.github.com>
2025-11-22 17:34:01 -0600

    Add lambda function and update file structure (#3)
    * add lambda
    
    * add lambda
    
    * add lambda
    
    * add tags
    
    * add tags
    
    * update build steps
    
    * change zip location
    
    * tf workflow update
    
    * tf workflow update
    
    * hardcode lambda for now
    
    * get forntend bucket name add back
    
    * oops forgot to swap the name
    
    * add outputs to infra job
    
    * add outputs to infra job
    
    * add aws config
    
    * add docs
    
    * add source code directory
    
    * seperate out tf components to seperate files
    
    * add default value for lambda zip
    
    * can it find it now
    
    * change pathing
    
    * cleanup

---
+++ terraform/lambda.tf
@@ -0,0 +1,55 @@
+# -------------------------
+# Lambda IAM Role
+# -------------------------
+resource "aws_iam_role" "lambda_exec" {
+  name = "transaction_handler_lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    application = "plantpass"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# -------------------------
+# Lambda Function
+# -------------------------
+resource "aws_lambda_function" "transaction_handler" {
+  function_name = "TransactionHandler"
+  filename      = var.lambda_zip_path
+  handler       = "lambda_handler.lambda_handler"
+  runtime       = "python3.11"
+  role          = aws_iam_role.lambda_exec.arn
+
+  tags = {
+    application = "plantpass"
+  }
+}
+
+resource "aws_lambda_function_url" "transaction_handler_url" {
+  function_name      = aws_lambda_function.transaction_handler.function_name
+  authorization_type = "NONE"
+}
+
+output "lambda_function_url" {
+  value = aws_lambda_function_url.transaction_handler_url.function_url
+}
+
+variable "lambda_zip_path" {
+  type        = string
+  description = "Path to Lambda ZIP relative to Terraform working directory"
+  default     = "lambda_package.zip"
+}
