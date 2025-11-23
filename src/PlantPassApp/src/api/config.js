b0e12e804b88fe5aa4b6442cfd7a4a8cbcff8038
Joseph Ku <josephku825@gmail.com>
2025-11-22 20:10:38 -0600

    remove trailing/

--- src/PlantPassApp/src/api/config.js
+++ src/PlantPassApp/src/api/config.js
@@ -3,4 +3,4 @@
 /**
  * Define the API Gateway base URL here
  */
-export const API_URL = "https://y5kg6dk6p3.execute-api.us-east-1.amazonaws.com/";
+export const API_URL = "https://y5kg6dk6p3.execute-api.us-east-1.amazonaws.com";
