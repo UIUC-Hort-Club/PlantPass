71561927428af5fe70f99497ff491109b8099442
Joseph Ku <josephku825@gmail.com>
2025-11-22 19:30:02 -0600

    name lambda ref

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -9,7 +9,7 @@ resource "aws_apigatewayv2_api" "frontend_api" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.frontend_api.id
   integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.my_lambda.arn
+  integration_uri  = aws_lambda_function.transaction_handler.arn
   payload_format_version = "2.0"
 }
 
