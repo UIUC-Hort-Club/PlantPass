908c8f60645f32dee0e0b82a9f80726fb446bd48
Joseph Ku <josephku825@gmail.com>
2026-02-19 23:59:22 -0600

    Add cloudfront_distribution_id output

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -66,3 +66,8 @@ output "cloudfront_domain_name" {
   value       = aws_cloudfront_distribution.frontend.domain_name
   description = "The CloudFront distribution domain name"
 }
+
+output "cloudfront_distribution_id" {
+  value       = aws_cloudfront_distribution.frontend.id
+  description = "The CloudFront distribution ID"
+}
